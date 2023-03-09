b05914be051be20dade0ec41ed44d80b2d922844
Kreuz45 <kreuz45@kreuz45.com>
2023-03-09 15:00:15 +0900

    feat: select_with_template

--- bqhus/select.py
+++ bqhus/select.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from jinja2 import Template, Environment, FileSystemLoader
 
 
 class Select:
@@ -16,3 +17,13 @@ def select(client, query, query_parameters=[]):
     job_config = bigquery.QueryJobConfig(query_parameters=query_parameters)
     query_job = client.query(query, job_config=job_config)
     return Select(query_job=query_job)
+
+
+def select_with_template(
+    client, template_dir, template_name, template_parameters={}, query_parameters=[]
+):
+    env = Environment(loader=FileSystemLoader(template_dir, encoding="utf8"))
+    tmpl = env.get_template(template_name)
+    query = tmpl.render(template_parameters)
+
+    return select(client, query, query_parameters)
