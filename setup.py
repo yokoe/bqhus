c2ce0b605eb0f3d36935f304c411872b3d59b232
Kreuz45 <kreuz45@kreuz45.com>
2023-05-17 19:02:43 +0900

    chore: Version 0.1.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ AUTHOR_EMAIL = "kreuz45@kreuz45.com"
 URL = "https://github.com/yokoe/bqhus"
 LICENSE = "MIT"
 DOWNLOAD_URL = URL
-VERSION = "0.1.9"
+VERSION = "0.1.10"
 PYTHON_REQUIRES = ">=3.9"
 INSTALL_REQUIRES = [
     "google-cloud-bigquery>=3.4.0",
